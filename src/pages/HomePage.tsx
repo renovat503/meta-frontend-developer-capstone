da6d25163e479d2d052d1dd5d91828926342638b
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-09 15:18:21 +0100

    Created the home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import { About, Hero, Specials, Testimonials } from "components";
+
+const HomePage = () => {
+  return (
+    <main>
+      <Hero />
+      <Specials />
+      <Testimonials />
+      <About />
+    </main>
+  );
+};
+
+export default HomePage;
