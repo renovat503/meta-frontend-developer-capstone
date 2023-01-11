19c3d5c7bbf28c4204da3692fd2dc87d777c76e7
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-11 17:27:26 +0100

    Added the booking confimation route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, BookingPage } from "pages";
+import { HomePage, BookingPage, ConfirmedBooking } from "pages";
 
 const RootRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/booking" element={<BookingPage />} />
+      <Route path="/booking-confirmation" element={<ConfirmedBooking />} />
     </Routes>
   );
 };
