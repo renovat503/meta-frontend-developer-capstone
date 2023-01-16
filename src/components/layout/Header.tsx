c5053a8a03462aa7ba924d2b9da6c5b1e4a9d4bf
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-16 09:51:00 +0100

    Created `Header.tsx`

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import {
+  Box,
+  Burger,
+  Button,
+  Container,
+  Group,
+  Header,
+  Image,
+  createStyles,
+} from "@mantine/core";
+import { colors } from "src/theme";
+
+const navLinks = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Menu",
+    link: "/menu",
+  },
+  {
+    title: "Reservations",
+    link: "/reservations",
+  },
+  {
+    title: "Order Online",
+    link: "/order-online",
+  },
+  {
+    title: "Login",
+    link: "/login",
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    borderBottom: 0,
+  },
+  inner: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+    "&:focus": {
+      backgroundColor: colors.primary,
+      color: theme.white,
+    },
+  },
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+const _Header = () => {
+  const { classes } = useStyles();
+
+  const [opened, setOpened] = React.useState(false);
+
+  const links = React.useMemo(
+    () =>
+      navLinks.map((link) => (
+        <Button
+          key={link.title}
+          className={classes.button}
+          variant="white"
+          radius="md"
+          fw="bold"
+          c="dark"
+        >
+          {link.title}
+        </Button>
+      )),
+    []
+  );
+
+  return (
+    <Header height={60} py="lg" className={classes.header}>
+      <Container className={classes.inner}>
+        <Box>
+          <Image src="/logo.svg" alt="Little Lemon" />
+        </Box>
+        <Group spacing="xs" noWrap>
+          {links}
+        </Group>
+        <Burger
+          opened={opened}
+          className={classes.burger}
+          onClick={() => setOpened((o) => !o)}
+        />
+      </Container>
+    </Header>
+  );
+};
+
+export default _Header;
