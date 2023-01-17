9c4d003ccb5d34c5faed5aa911d6462af7476275
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-17 08:59:40 +0100

    Exported `Specials` and `Card`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Layout } from "./layout/Layout";
 export { default as Header } from "./layout/Header";
 export { default as Footer } from "./layout/Footer";
+export { default as Specials } from "./Specials";
 export { default as Hero } from "./Hero";
+export { default as Card } from "./Card";
