d421aca3167e7559ea0058340eaf32ebc9d1fa43
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-17 09:42:21 +0100

    Exported `Testimonial` and `Testimonials`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as Layout } from "./layout/Layout";
 export { default as Header } from "./layout/Header";
 export { default as Footer } from "./layout/Footer";
+export { default as Testimonials } from "./Testimonials";
+export { default as Testimonial } from "./Testimonial";
 export { default as Specials } from "./Specials";
 export { default as Hero } from "./Hero";
 export { default as Card } from "./Card";
